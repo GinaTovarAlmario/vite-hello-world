465ef2be2cc6b91576eccbd4ac95a54f973d0a82
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-13 16:41:30 +0200

    first try using components and vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- 1-importo il file in cima al mio script e gli diamo un nome -->
+<!-- 2-lo inserisco nella lista dei componenti -->
+<!-- 2-lo inserisco come un self-closing tag in html -->
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components:{
+    AppTitle
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+<style scoped>
+</style>
